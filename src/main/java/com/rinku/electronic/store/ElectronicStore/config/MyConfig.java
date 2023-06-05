ffc201787cc6bf5917e8cee4f387fb87c715c277
Rinku2027 <rinkupatil.20@gmail.com>
2023-06-05 23:06:18 +0530

    Project code upto User Module

--- src/main/java/com/rinku/electronic/store/ElectronicStore/config/MyConfig.java
+++ src/main/java/com/rinku/electronic/store/ElectronicStore/config/MyConfig.java
@@ -3,6 +3,7 @@ package com.rinku.electronic.store.ElectronicStore.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class MyConfig {
@@ -11,6 +12,10 @@ public ModelMapper modelMapper(){
 
     return new ModelMapper();
 }
-
+@Bean
+public RestTemplate restTemplate()
+{
+    return new RestTemplate();
+}
 
 }
