fa672a3a21e00ea9ad38358c28c042356f5304ce
Rinku2027 <rinkupatil.20@gmail.com>
2023-06-12 19:10:20 +0530

    Creating Category and CategoryDto class

---
+++ src/main/java/com/rinku/electronic/store/ElectronicStore/dtos/CategoryDto.java
@@ -0,0 +1,28 @@
+package com.rinku.electronic.store.ElectronicStore.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDto {
+    private String CategoryId;
+    @NotBlank
+    @Size(min = 5, max = 15, message = "title must be within limit (min =5,max=15) .....!! ")
+    private String title;
+    @NotBlank
+    @Size(min = 10, max = 30, message = "description must be within min length 10 and max length 30")
+    private String description;
+    private String coverImage;
+
+
+}
