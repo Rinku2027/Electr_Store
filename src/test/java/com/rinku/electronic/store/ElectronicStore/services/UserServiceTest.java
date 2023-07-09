ee1256d0db30a77a0a5021b131cfd653ba591e42
Rinku2027 <rinkupatil.20@gmail.com>
2023-07-09 11:54:37 +0530

    Service layer test

--- src/test/java/com/rinku/electronic/store/ElectronicStore/services/UserServiceTest.java
+++ src/test/java/com/rinku/electronic/store/ElectronicStore/services/UserServiceTest.java
@@ -1,15 +1,47 @@
 package com.rinku.electronic.store.ElectronicStore.services;
 
+import com.rinku.electronic.store.ElectronicStore.entity.User;
 import com.rinku.electronic.store.ElectronicStore.repository.UserRepo;
 import com.rinku.electronic.store.ElectronicStore.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import javax.management.relation.Role;
+import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
-    @Autowired
+    @Mock
     private UserRepo userRepo;
+    @Autowired
+    @InjectMocks
+    private UserService userService;
+
+    User user;
+    Role role;
+    @BeforeEach
+    public void init()
+    {
+                User.builder().name("Rinku").
+                email("rinku@gmail.com").about("This is testing create method")
+                .gender("Male").imageName("abc.png")
+                .password("lcwd").roles(Set.of(role)).build();
+
+
+    }
+
+
+    //create user
+    public void createUserTest()
+    {
+        Mockito.when(userRepo.save(Mockito.any())).thenReturn();
+
+    }
+
 
 
 }
