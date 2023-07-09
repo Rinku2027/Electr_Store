5485b18debe718c9f5ea2e6194a5ad7069c0ea9f
Rinku2027 <rinkupatil.20@gmail.com>
2023-07-09 15:26:43 +0530

    UserControllerTest

---
+++ src/test/java/com/rinku/electronic/store/ElectronicStore/controllers/UserControllerTest.java
@@ -0,0 +1,216 @@
+package com.rinku.electronic.store.ElectronicStore.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rinku.electronic.store.ElectronicStore.dtos.PageableResponse;
+import com.rinku.electronic.store.ElectronicStore.dtos.UserDto;
+import com.rinku.electronic.store.ElectronicStore.entity.User;
+import com.rinku.electronic.store.ElectronicStore.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class UserControllerTest {
+    @MockBean
+    private UserService userServiceI;
+
+    @Autowired
+    private ModelMapper mapper ;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    User user;
+    @BeforeEach
+    void setUp() {
+        user = User.builder()
+                .about("testing for controller")
+                .name("Rinku")
+                .email("rinkupatil.20@gmail.com")
+                .gender("female")
+                .password("rinku")
+                .imageName("xyz.png")
+                .build();
+
+    }
+
+    @Test
+    void saveUserTest() throws Exception {
+        UserDto userdto = this.mapper.map(user, UserDto.class);
+        Mockito.when(userServiceI.createUser(Mockito.any())).thenReturn(userdto);
+
+        //request for url
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/user")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(convertObjectToJsonString(user))
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").exists());
+
+    }
+
+    //extra method for conversion
+    private String convertObjectToJsonString(Object user) {
+        try {
+            return new ObjectMapper().writeValueAsString(user);
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    @Test
+    void getUserByIdTest() throws Exception {
+        String userId ="avc";
+        UserDto userDto = this.mapper.map(user, UserDto.class);
+        Mockito.when(userServiceI.getUserById(Mockito.anyString())).thenReturn(userDto);
+
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/user/"+userId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isFound())
+                .andExpect(jsonPath("$.name").exists());
+
+    }
+
+    @Test
+    void getAllUsersTest() throws Exception {
+        UserDto user1 = UserDto.builder()
+                .about("testing for getAll controller method")
+                .name("pakhi")
+                .email("pakhi@gmail.com")
+                .gender("female")
+                .password("pakhi@123")
+                .imageName("xyz.png").build();
+
+        UserDto user2 = UserDto.builder()
+                .about("testing for getAll controller method")
+                .name("satish")
+                .email("satish@gmail.com")
+                .gender("male")
+                .password("shptl@123")
+                .imageName("abc.png").build();
+
+        UserDto user3 = UserDto.builder()
+                .about("testing for getAll controller method")
+                .name("rachana")
+                .email("rachana@gmail.com")
+                .gender("female")
+                .password("rachana@123")
+                .imageName("abc.png").build();
+
+        PageableResponse<UserDto> pageableResponse = new PageableResponse<>();
+
+        pageableResponse.setLastPage(false);
+        pageableResponse.setTotalElements(2000);
+        pageableResponse.setPageNumber(50);
+        pageableResponse.setContent(Arrays.asList(user1,user2,user3));
+        pageableResponse.setTotalPages(200);
+        pageableResponse.setPageSize(20);
+
+        Mockito.when(userServiceI.getAllUser(Mockito.anyInt(),Mockito.anyInt(),Mockito.anyString(),Mockito.anyString())).thenReturn(pageableResponse);
+
+        //request for url
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+
+    }
+
+    @Test
+    void updateUserTest() throws Exception {
+
+        String userId ="abc";
+
+        UserDto userdto = this.mapper.map(user, UserDto.class);
+        Mockito.when(userServiceI.updateUser(Mockito.any(),Mockito.anyString())).thenReturn(userdto);
+
+        //request for url
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.put("/api/user/"+userId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(convertObjectToJsonString(user))
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").exists());
+
+    }
+
+
+    @Test
+    void deleteUserTest() {
+    }
+
+    @Test
+    void getUserByEmailTest() throws Exception {
+        String emailId ="rinkupatil.20@gmail.com";
+        UserDto userDto = this.mapper.map(user, UserDto.class);
+        Mockito.when(userServiceI.getUserByEmail(Mockito.anyString())).thenReturn(userDto);
+
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/user/email/"+emailId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").exists());
+    }
+
+
+
+
+
+    @Test
+    void searchUserTest() throws Exception {
+
+        String keyword ="image";
+        UserDto user1 = UserDto.builder()
+                .about("testing for getAll controller method")
+                .name("satish")
+                .email("shptl@gmail.com")
+                .gender("male")
+                .password("sh@123")
+                .imageName("xyz.png").build();
+
+        UserDto user2 = UserDto.builder()
+                .about("testing for getAll controller method")
+                .name("rachana")
+                .email("rachana@gmail.com")
+                .gender("female")
+                .password("rachana@123")
+                .imageName("abc.png").build();
+
+        Mockito.when(userServiceI.searchUser(Mockito.anyString())).thenReturn(Arrays.asList(user1,user2));
+
+        //request for url
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/user/search/"+keyword)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isFound());
+
+
+    }
+}
--- src/test/java/com/rinku/electronic/store/ElectronicStore/services/UserServiceTest.java
+++ src/test/java/com/rinku/electronic/store/ElectronicStore/services/UserServiceTest.java
@@ -1,47 +1,184 @@
 package com.rinku.electronic.store.ElectronicStore.services;
 
+import com.rinku.electronic.store.ElectronicStore.dtos.PageableResponse;
+import com.rinku.electronic.store.ElectronicStore.dtos.UserDto;
 import com.rinku.electronic.store.ElectronicStore.entity.User;
 import com.rinku.electronic.store.ElectronicStore.repository.UserRepo;
 import com.rinku.electronic.store.ElectronicStore.service.UserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
 import javax.management.relation.Role;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
-    @Mock
+    @MockBean
     private UserRepo userRepo;
+
     @Autowired
-    @InjectMocks
     private UserService userService;
 
+    @Autowired
+    private ModelMapper mapper;
+
     User user;
-    Role role;
+
     @BeforeEach
-    public void init()
-    {
-                User.builder().name("Rinku").
-                email("rinku@gmail.com").about("This is testing create method")
-                .gender("Male").imageName("abc.png")
-                .password("lcwd").roles(Set.of(role)).build();
+    void init() {
+        user = User.builder()
+                .about("testing for userCreate method")
+                .name("Rinku")
+                .email("rinkupatil.20@gmail.com")
+                .gender("female")
+                .password("rinku@123")
+                .imageName("xyz.png").build();
+    }
 
+    @Test
+    void saveUser() {
+
+        //stubbing
+        Mockito.when(userRepo.save(Mockito.any())).thenReturn(user);
+        //actual method calling
+        UserDto userDto = userService.createUser(this.mapper.map(user, UserDto.class));
+        //matching using assertion
+        Assertions.assertNotNull(userDto);
+        Assertions.assertEquals("Rinku", userDto.getName(), "UserName not matched ");
 
     }
 
+    @Test
+    void getUserById() {
 
-    //create user
-    public void createUserTest()
-    {
-        Mockito.when(userRepo.save(Mockito.any())).thenReturn();
+        String userId = "xyz";
+        //stubbing
+        Mockito.when(userRepo.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+        //actual method calling
+        UserDto userdto = userService.getUserById(userId);
+        //matching using assertion
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals(user.getGender(), userdto.getGender(), "Gender not matched ");
+    }
+
+    @Test
+    void getAllUsers() {
+        User user1 = User.builder()
+                .about("testing for getAllUser method")
+                .name("pakhi")
+                .email("pakhi@gmail.com")
+                .gender("female")
+                .password("pakhi@123")
+                .imageName("pakhi.png").build();
+
+        User user2 = User.builder()
+                .about("testing for getAllUser method")
+                .name("Satish")
+                .email("satish@gmail.com")
+                .gender("male")
+                .password("shptl@123")
+                .imageName("shatish.png").build();
+
+        List<User> list = Arrays.asList(user, user1, user2);
+
+        Page<User> page = new PageImpl<>(list);
+
+        //stubbing
+        Mockito.when(userRepo.findAll((Pageable) Mockito.any())).thenReturn(page);
+        //actual method calling
+        PageableResponse<UserDto> allUsers = userService.getAllUser(1, 2, "state", "desc");
+        //matching using assertion
+        Assertions.assertEquals(3, allUsers.getContent().size(), "Size not validate");
 
     }
 
+    @Test
+    void updateUser() {
+        String userId = "abc";
+        UserDto userDto = UserDto.builder()
+                .about(" updated testing for updateProduct method")
+                .name("Shweta")
+                .email("rinkupatil.20@gmail.com")
+                .gender("female")
+                .password("shweta@123")
+                .imageName("shweta.png").build();
+
+        //stubbing
+        Mockito.when(userRepo.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+        Mockito.when(userRepo.save(Mockito.any())).thenReturn(user);
+        //actual method calling
+        UserDto userDto1 = userService.updateUser(userDto, userId);
+        //matching using assertion
+        Assertions.assertEquals("shweta", userDto1.getName(), "username not validate");
+        Assertions.assertNotNull(userDto1);
+
+    }
+
+    @Test
+    void deleteUser() {
+
+        String userId = "abc";
+        //stubbing
+        Mockito.when(userRepo.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+        //actual method calling
+        userService.deleteUser(userId);
+        //verifying result
+        Mockito.verify(userRepo, Mockito.times(1)).delete(user);
+    }
+
+    @Test
+    void getUserByEmail() {
+
+        String email = "rinkupatil.20@gmail.com";
+        //stubbing
+        Mockito.when(userRepo.findByEmail(email)).thenReturn(Optional.of(user));
+        //actual method calling
+        UserDto userdto = userService.getUserByEmail(email);
+        //matching using assertion
+        Assertions.assertEquals(user.getEmail(), userdto.getEmail(), "Email not matched ");
+    }
+
+    @Test
+    void searchUser() {
+
+        User user1 = User.builder()
+                .about("testing for search user method")
+                .name("Shweta")
+                .email("Shweta@gmail.com")
+                .gender("female")
+                .password("shweta@123")
+                .imageName("pankaj.png").build();
+
+        User user2 = User.builder()
+                .about("testing for search user method")
+                .name("rachana")
+                .email("rachana@gmail.com")
+                .gender("female")
+                .password("rachana@123")
+                .imageName("rachna.png").build();
 
 
+        String keyword = "rachana";
+        //stubbing
+        Mockito.when(userRepo.findByNameContaining(keyword)).thenReturn(Arrays.asList(user, user1, user2));
+        //actual method calling
+        List<UserDto> userDto = userService.searchUser(keyword);
+        //matching using assertion
+        Assertions.assertEquals(3, userDto.size(), "size not matched");
+    }
 }
