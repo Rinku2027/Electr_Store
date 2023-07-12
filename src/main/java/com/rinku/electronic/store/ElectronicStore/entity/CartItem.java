bee2f122aea4a17f813f4c548093140ba1cecd86
Rinku2027 <rinkupatil.20@gmail.com>
2023-07-12 22:21:20 +0530

    cart file

--- src/main/java/com/rinku/electronic/store/ElectronicStore/entity/CartItem.java
+++ src/main/java/com/rinku/electronic/store/ElectronicStore/entity/CartItem.java
@@ -16,11 +16,14 @@ public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int cartitems;
+
     @OneToOne
     @JoinColumn(name = "product_id")
     private Product product;
+
     private int quantity;
     private int totalprice;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private Cart cart;
 
