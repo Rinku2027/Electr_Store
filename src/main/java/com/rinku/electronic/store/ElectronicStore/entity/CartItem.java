a15d8634d3f31457b89c08bce30bac788af59547
Rinku2027 <rinkupatil.20@gmail.com>
2023-07-12 09:56:13 +0530

    Cart Item file

---
+++ src/main/java/com/rinku/electronic/store/ElectronicStore/entity/CartItem.java
@@ -0,0 +1,28 @@
+package com.rinku.electronic.store.ElectronicStore.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cartitems;
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    private int quantity;
+    private int totalprice;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Cart cart;
+
+
+}
