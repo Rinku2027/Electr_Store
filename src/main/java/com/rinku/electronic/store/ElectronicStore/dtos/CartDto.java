aa279a80fd30b9b33709b5fda06301199a66c80d
Rinku2027 <rinkupatil.20@gmail.com>
2023-07-12 09:55:44 +0530

    Cart Dto Module

---
+++ src/main/java/com/rinku/electronic/store/ElectronicStore/dtos/CartDto.java
@@ -0,0 +1,20 @@
+package com.rinku.electronic.store.ElectronicStore.dtos;
+
+import com.rinku.electronic.store.ElectronicStore.entity.CartItem;
+import com.rinku.electronic.store.ElectronicStore.entity.User;
+
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class CartDto {
+    private String cartId;
+
+    private Date createdAt;
+    @OneToOne
+    private UserDto user;
+    @OneToMany(mappedBy = "cart")
+    private List<CartItem> items=new ArrayList<>();
+}
