3c6a1e13f8e3180efdbecc4540c6020872491dad
Rinku2027 <rinkupatil.20@gmail.com>
2023-07-15 09:33:27 +0530

    updating cart module

--- src/main/java/com/rinku/electronic/store/ElectronicStore/entity/Cart.java
+++ src/main/java/com/rinku/electronic/store/ElectronicStore/entity/Cart.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -16,11 +17,12 @@ import java.util.List;
 
 public class Cart {
     @Id
-    private String cartId;
-    private Date createdAt;
-    @OneToOne
-    private User user;
-    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    private List<CartItem> items=new ArrayList<>();
+   private String cartId;
+  private Date createdAt;
+ @OneToOne
+ private User user;
+ @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+ private List<CartItem> items=new ArrayList<>();
 
 }
+
