65287278637821839a4dbd2d8e8c38b81dc814e4
Rinku2027 <rinkupatil.20@gmail.com>
2023-07-16 15:48:53 +0530

    UserService

--- src/test/java/com/rinku/electronic/store/ElectronicStore/ElectronicStoreApplicationTests.java
+++ src/test/java/com/rinku/electronic/store/ElectronicStore/ElectronicStoreApplicationTests.java
@@ -1,13 +1,10 @@
 package com.rinku.electronic.store.ElectronicStore;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class ElectronicStoreApplicationTests {
-
 	@Test
 	void contextLoads() {
+	//	System.out.println("Testing project");
 	}
-
 }
