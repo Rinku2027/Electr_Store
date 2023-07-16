89a203db543482954947e2231e7dff8dd335265d
Rinku2027 <rinkupatil.20@gmail.com>
2023-07-16 15:46:04 +0530

    changes in CartItemDto File

--- src/main/java/com/rinku/electronic/store/ElectronicStore/dtos/CartItemDto.java
+++ src/main/java/com/rinku/electronic/store/ElectronicStore/dtos/CartItemDto.java
@@ -2,11 +2,18 @@ package com.rinku.electronic.store.ElectronicStore.dtos;
 
 import com.rinku.electronic.store.ElectronicStore.entity.Cart;
 import com.rinku.electronic.store.ElectronicStore.entity.Product;
+import lombok.*;
+
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CartItemDto {
     private int cartitems;
     private ProductDto productdto;
